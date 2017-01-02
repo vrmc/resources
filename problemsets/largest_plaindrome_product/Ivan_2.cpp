1331721cd0d7ac4bad0026f040973a5188d70ca8
IvanFengJK <ivanfengjunkai1997114@gmail.com>
2017-01-03 00:41:37 +0800

    A more optimised solution
    After reading from the document provided from Euler project.

---
+++ problemsets/largest_plaindrome_product/Ivan_2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string Reverse(string str){
+    string reverseString;
+    
+    for(long i = str.size() - 1;i >= 0; --i){
+        reverseString += str[i];
+    }
+    return reverseString;
+}
+
+int main() {
+    int factor =90;
+    int answer = 0;
+    for (int a=990; a>100; a=11*factor) {
+        for (int b=999; b>100; b--) {
+            int sum = a*b;
+            string product = to_string(sum);
+            int temp = stoi(Reverse(product));
+            if (temp==sum && sum>answer) {
+                answer=sum;
+            }
+            if (answer>sum) {
+                break;
+            }
+        }
+        factor++;
+    }
+    cout << answer<<endl;
+    return 0;
+}
